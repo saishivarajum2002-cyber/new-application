fb6b59c1de718a3b56707d988a65611be535d103
anitha <anitha.m1117@gmail.com>
2026-05-01 20:04:02 +0530

    Fix Android structural errors and add Gradle boilerplate

---
+++ sarah-pro-native/android/app/src/main/java/com/propedge/sarahpro/MainActivity.java
@@ -0,0 +1,23 @@
+package com.propedge.sarahpro;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
+public class MainActivity extends ReactActivity {
+
+  @Override
+  protected String getMainComponentName() {
+    return "SarahPro";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        // If you opted-in to the New Architecture, we enable the Fabric Renderer.
+        DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  }
+}
